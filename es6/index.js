7728b62626a366352275259d49f6fb0f7b14f3d8
Adeleke Hammed <lekhad19@gmail.com>
2024-05-17 15:25:25 +0100

    displaying projects

---
+++ es6/index.js
@@ -0,0 +1,11 @@
+let word1 = 'Dylan'; 
+let word2 = 'Israel'; 
+let num1 =2; 
+let num2 = 3; 
+
+// const fullName = `${num1 + num2} ${word2}`; 
+// console.log(fullName); 
+
+let example = 'Hello \n' + 'world';
+console.log(example);
+document.getElementById('example').innerText = example; 
